a6545c4f1baebda258a90430cb6202911430913a
Kristopher Kelly <kmk1976@gmail.com>
2021-03-13 06:00:35 -0700

    Added game cards and PowerBall Number generation

--- assets/app.js
+++ assets/app.js
@@ -1,38 +1,130 @@
-// 1. Create the button
+// Create the button
 var button = document.createElement("button");
-button.innerHTML = "PowerBall";
+button.innerHTML = "Play";
 
-// 2. Append somewhere
-var body = document.getElementById("gameselection")[0];
-gameselection.appendChild(button);
+// Append somewhere
+var body = document.getElementById("pballgame")[0];
+pballgame.appendChild(button);
 
-// 3. Add event handler
+// Add event handler
 button.addEventListener("click", function () {
-    alert("You selected PowerBall");
+    var elem = document.getElementById("box");
+    elem.style.transition = "all 2s ease-in-out";
+    elem.style.height = "150px";
 });
 
-// 1. Create the button
+function slideDown(box) {
+
+}
+
+function slideUp(box) {
+
+}
+
+
+
+//Create the button
 var button = document.createElement("button");
-button.innerHTML = "Mega Millions";
+button.innerHTML = "Play";
 
 // 2. Append somewhere
-var body = document.getElementById("gameselection")[0];
-gameselection.appendChild(button);
+var body = document.getElementById("mmgame")[0];
+mmgame.appendChild(button);
 
-// 3. Add event handler
+//Add event handler
 button.addEventListener("click", function () {
-    alert("You selected the Mega Millons");
+    var elem = document.getElementById("box2");
+    elem.style.transition = "all 2s ease-in-out";
+    elem.style.height = "150px";
 });
 
-// 1. Create the button
+function slideDown(box2) {
+
+}
+
+
+//Create the button
 var button = document.createElement("button");
-button.innerHTML = "The Pick";
+button.innerHTML = "Play";
 
-// 2. Append somewhere
-var body = document.getElementById("gameselection")[0];
-gameselection.appendChild(button);
+//Append somewhere
+var body = document.getElementById("thepickgame")[0];
+thepickgame.appendChild(button);
 
-// 3. Add event handler
+//Add event handler
 button.addEventListener("click", function () {
-    alert("You selected the Pick");
+    var elem = document.getElementById("box3");
+    elem.style.transition = "all 2s ease-in-out";
+    elem.style.height = "150px";
 });
+
+function slideDown(box3) {
+
+}
+
+function slideUp(box) {
+    var elem = document.getElementById(box);
+    elem.style.transition = "all 2s ease-in-out";
+    elem.style.height = "0px";
+
+}
+function slideDown(box) {
+    var elem = document.getElementById(box);
+    elem.style.transition = "all 2s ease-in-out";
+    elem.style.height = "150px";
+}
+
+function slideUp(box2) {
+    var elem = document.getElementById(box2);
+    elem.style.transition = "all 2s ease-in-out";
+    elem.style.height = "0px";
+
+}
+function slideDown(box2) {
+    var elem = document.getElementById(box2);
+    elem.style.transition = "all 2s ease-in-out";
+    elem.style.height = "150px";
+}
+
+function slideUp(box3) {
+    var elem = document.getElementById(box3);
+    elem.style.transition = "all 2s ease-in-out";
+    elem.style.height = "0px";
+
+}
+function slideDown(box3) {
+    var elem = document.getElementById(box3);
+    elem.style.transition = "all 2s ease-in-out";
+    elem.style.height = "150px";
+}
+
+function generatepballnum(size, lowest, highest) {
+	var numbers = [];
+	for(var i = 0; i < size; i++) {
+		var add = true;
+		var randomNumber = Math.floor(Math.random() * highest) + 1;
+		for(var y = 0; y < highest; y++) {
+			if(numbers[y] == randomNumber) {
+				add = false;
+			}
+		}
+		if(add) {
+			numbers.push(randomNumber);
+		} else {
+			i--;
+		}
+	}
+  
+	var highestNumber = 0;
+	for(var m = 0; m < numbers.length; m++) {
+		for(var n = m + 1; n < numbers.length; n++) {
+			if(numbers[n] < numbers[m]) {
+				highestNumber = numbers[m];
+				numbers[m] = numbers[n];
+				numbers[n] = highestNumber;
+			}
+		}
+	}
+  
+	document.getElementById("pballgamenum").innerHTML = numbers.join(" - ");
+}
